126ff97b2dec5001b09693b55f6ae267a71a0832
Thiago Mota <thiagomota99@outlook.com>
2022-03-19 18:56:01 -0300

    Primeira Classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
+		
+		System.out.println("Testando a classe Posicao");
+		Posicao posicao = new Posicao(3,5);
+		
+		System.out.println(posicao);
 	}
 
 }
